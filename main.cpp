c677acd60af02e92fa010359069bd8d9b6b67e93
SchrodingerZhu <i@zhuyi.fan>
2021-01-22 15:56:43 +0800

    update fend

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ auto test = "begin\n"
             "   a16 := a + 1;\n"
             "   a17 := a + 1;\n"
             "   a18 := a + 1;\n"
-            "   write(a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 + a13 + a14 + a15 + a16 + a17 + a18);\n"
+            "   write(a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11 + a12 + a13 + a14 + a15 + a16 + a17 + a18 + 1);\n"
             "end\n";
 
 int main() {
